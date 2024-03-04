a58ee54bd93adad68f3148d364987072ce331b07
Nitin Kumar Pal <nitinpal900@gmail.com>
2024-03-04 22:03:14 +0530

    auth-service-done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+  console.log('hello');
+  return (
+    <>
+    <h1>Hello</h1>
+    </>
+  )
+}
+
+export default App
